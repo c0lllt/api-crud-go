6f382e2dab2e1661565eab42604c3d36ca5a7d6a
Paulo Nunes <paulo.hnrnunes@gmail.com>
2024-11-25 15:23:16 -0300

    Atualizando mascara do CPF com tudo certo

--- models/modelos.go
+++ models/modelos.go
@@ -3,7 +3,7 @@ package models
 type Cliente struct {
 	ID    int
 	Nome  string `json:"nome" binding:"required,min=4"`
-	Cpf   string `json:"cpf" binding:"required,len=14"`
+	Cpf   string `json:"cpf" binding:"required,len=14"` //mudando para 14 para nao ter problema com a mascara do front
 	Senha string `json:"senha" binding:"required,min=6,containsany=!@#$%"`
 }
 
